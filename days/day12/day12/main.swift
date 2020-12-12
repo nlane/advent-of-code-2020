2ff1e03654a26bbe0859ee952ba571a937bf8b6a
Natalie Lane <nlane@udel.edu>
2020-12-12 12:43:21 -0500

    adds pt 2 day 12

--- days/day12/day12/main.swift
+++ days/day12/day12/main.swift
@@ -58,5 +58,53 @@ func followNavigation(directions: [ ( Character, Int ) ] ) -> Int {
     return abs(north) + abs(east)
 }
 
+func getNewDir(currDir: Character, clockwise: Bool, degrees: Int) -> Character {
+    let fourDirs: [Character] = ["E", "S", "W", "N"]
+    var idx = degrees
+    if clockwise {
+        idx = idx + fourDirs.firstIndex(of: currDir)!
+        idx = idx >= 4 ? idx - 4 : idx
+    } else {
+        idx = fourDirs.firstIndex(of: currDir)! - idx
+        idx = idx < 0 ? idx + 4 : idx
+    }
+    return fourDirs[idx]
+}
+
+func followNavigationPt2(directions: [ (Character, Int) ]) -> Int {
+    var boatN = 0, boatE = 0
+    var waypoint: [ (dir: Character, steps: Int) ] = [ ("N", 1), ("E", 10) ]
+    for (dir, steps) in directions {
+        if ["E", "S", "W", "N"].contains(dir) { // move waypoint
+            var newSteps = steps
+            if waypoint[0].dir == "S" && (dir == "N" || dir == "S") {
+                newSteps = -steps
+            } else if waypoint[1].dir == "W" && (dir == "E" || dir == "W") {
+                newSteps = -steps
+            }
+            var nSteps = waypoint[0].steps
+            var eSteps = waypoint[1].steps
+            (nSteps, eSteps) = navigate(dir: dir, north: &nSteps, east: &eSteps, steps: newSteps)
+            waypoint = [ (waypoint[0].dir, nSteps), (waypoint[1].dir, eSteps)]
+        } else if dir == "R" || dir == "L" { // rotate waypoint
+            let degrees = steps / 90
+            let newDirOne = getNewDir(currDir: waypoint[0].dir, clockwise: dir == "R", degrees: degrees)
+            let newDirTwo = getNewDir(currDir: waypoint[1].dir, clockwise: dir == "R", degrees: degrees)
+            if newDirOne == "N" || newDirOne == "S" {
+                waypoint = [ (newDirOne, waypoint[0].steps), (newDirTwo, waypoint[1].steps) ]
+            } else {
+                waypoint = [ (newDirTwo, waypoint[1].steps), (newDirOne, waypoint[0].steps) ]
+            }
+        } else { // move ship to waypoint, move waypoint out
+            for _ in 1...steps {
+                (boatN, boatE) = navigate(dir: waypoint[0].dir, north: &boatN, east: &boatE, steps: waypoint[0].steps)
+                (boatN, boatE) = navigate(dir: waypoint[1].dir, north: &boatN, east: &boatE, steps: waypoint[1].steps)
+            }
+        }
+    }
+    return abs(boatN) + abs(boatE)
+}
+
 let directions = process(input: input)
 print(followNavigation(directions: directions))
+print(followNavigationPt2(directions: directions))
