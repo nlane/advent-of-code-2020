ea3d4a1f6f3cfb3f41eea5259887e00e4425c6b8
Natalie Lane <nlane@udel.edu>
2020-12-13 11:35:13 -0500

    adds day 13

---
+++ days/day13/day13/main.swift
@@ -0,0 +1,70 @@
+//
+//  main.swift
+//  day13
+//
+//  Created by Natalie Lane on 12/13/20.
+//
+
+import Foundation
+
+func process(input: String) -> (Int, [ Int ]) {
+    let split = input.split(separator: "\n")
+    var buses: [ Int ] = []
+    for bus in split[1].split(separator: ",") {
+        if bus != "x" {
+            buses.append(Int(String(bus))!)
+        }
+    }
+    return (Int(split[0])!, buses)
+}
+
+func processPt2(input: String) -> ( [ String ] ) {
+    let split = input.split(separator: "\n")
+    let output = split[1].components(separatedBy: ",")
+    return output
+}
+
+func findNextBus(startTime: Int, buses: [ Int ]) -> Int {
+    var currTime = startTime - 1
+    var nextBus = -1
+    let sortedBuses = buses.sorted()
+    while nextBus < 0 {
+        currTime += 1
+        for bus in sortedBuses {
+            if currTime % bus == 0 {
+                nextBus = bus
+                break
+            }
+        }
+    }
+    return (currTime - startTime) * nextBus
+}
+
+func calculateOffsets(buses: [ String ]) -> [ Int: Int ] {
+    var offsets: [ Int: Int] = [:]
+    for (idx, bus) in buses.enumerated() {
+        if bus != "x" {
+            offsets[Int(bus)!] = idx
+        }
+    }
+    return offsets
+}
+
+func part2(buses: [ Int ], offsets: [ Int : Int ]) -> Int {
+    var currTime = 100000000000000
+    var step = 1
+    for i in 0..<buses.count {
+        let currBus = buses[i]
+        while ((currTime + offsets[currBus]!) % currBus != 0) {
+            currTime += step
+        }
+        step *= currBus
+    }
+    return currTime
+}
+
+let (startTime, activeBuses) = process(input: input)
+print(findNextBus(startTime: startTime, buses: activeBuses))
+let fullBuses = processPt2(input: input)
+let offsets = calculateOffsets(buses: fullBuses)
+print(part2(buses: activeBuses, offsets: offsets))
---
+++ days/day13/day13/puzzleInput.swift
@@ -0,0 +1,13 @@
+//
+//  puzzleInput.swift
+//  day13
+//
+//  Created by Natalie Lane on 12/13/20.
+//
+
+import Foundation
+
+let input = """
+1000053
+19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,547,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,17
+"""
